b872176872857b46fd9b03cc8236534e3529ae89
Antonio Carlos <antonioccsjr@hotmail.com>
2024-04-17 00:08:43 -0300

    Criação da rota Search

--- routes/Search/Search.js
+++ routes/Search/Search.js
@@ -1,19 +1,18 @@
-const express = require ("express");
+const express = require("express");
 const router = express();
 const bodyParser = require("body-parser");
-
 const axios = require('axios');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.post('/search', async(req, res) => {
+router.post('/search', async (req, res) => {
     try {
         const searchItem = req.body.searchItem;
-        console.log(searchItem)
+        console.log('searchItem:', searchItem);
 
-        const index = 0; 
-        const limit = 2; 
+        const index = 0;
+        const limit = 4;
         const format = 'json';
 
         const apiUrl = `https://api.deezer.com/search?q=${searchItem}&index=${index}&limit=${limit}&output=${format}`;
@@ -21,26 +20,34 @@ router.post('/search', async(req, res) => {
         const searchTracks = async () => {
             try {
                 const response = await axios.get(apiUrl);
-                const tracks = response.data.data; 
-                const formattedTracks = tracks.map(track => ({
-                    title: track.title, 
-                    preview: track.preview, 
-                    artist: track.artist.name, 
-                    albumCover: track.album.cover_big,
-                }));
-                console.log(formattedTracks)
-                res.status(200).json(formattedTracks);
+                console.log('Response data:', response.data);
+                if (response.data && response.data.data) {
+                    const tracks = response.data.data;
+                    console.log('Tracks:', tracks);
+                    const formattedTracks = tracks.map(track => ({
+                        title: track.title,
+                        preview: track.preview,
+                        artist: track.artist.name,
+                        albumCover: track.album.cover_big,
+                        artistPic: track.artist.picture_big,
+                        duration: track.duration,
+                    }));
+                    res.status(200).json(formattedTracks);
+                } else {
+                    console.error('Dados de resposta da API estão incompletos ou ausentes.');
+                    res.status(500).json({ error: 'Erro ao processar a resposta da API.' });
+                }
             } catch (error) {
                 console.error('Erro ao fazer a solicitação:', error);
+                res.status(500).json({ error: 'Erro ao fazer a solicitação para a API.' });
             }
         };
-        
-        await searchTracks();
-    }
-    catch (err) {
-        console.log('Erro ao realizar a busca: ', err)
-    }
-})
 
+        await searchTracks();
+    } catch (err) {
+        console.log('Erro ao realizar a busca: ', err)
+        res.status(500).json({ error: 'Erro interno ao realizar a busca.' });
+    }
+});
 
 module.exports = router;
